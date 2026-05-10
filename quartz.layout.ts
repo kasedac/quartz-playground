c01408947628e6e974c9ea03f1f35acd475c52b6
Choji Kaseda <kasedac@aga-ye.com>
2026-05-10 12:15:31 +0900

    feat: update footer links to GitHub (personal) and X (Twitter)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,8 +17,8 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "GitHub": "https://github.com/kasedac",
+      "X(Twitter):": "https://x.com/kasedac/",
     },
   }),
 }
